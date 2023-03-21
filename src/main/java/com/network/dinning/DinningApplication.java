8c23fe9e6f4f18048958bc4f32b4dc6179ebd62b
gonzruga <grugambwa@gmail.com>
2023-03-21 11:41:25 +0200

    Initial Version

---
+++ src/main/java/com/network/dinning/DinningApplication.java
@@ -0,0 +1,13 @@
+package com.network.dinning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DinningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DinningApplication.class, args);
+	}
+
+}
